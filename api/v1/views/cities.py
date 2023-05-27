7b36beb4c33360e1b88bcad9ada769656bdc5397
Bantamlak <bantamlak29@gmail.com>
2023-05-27 11:55:02 +0300

    status code returned

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -69,4 +69,4 @@ def update_city(city_id):
         if k not in ['id', 'state_id', 'created_at', 'updated_at']:
             city.k = v
     storage.save()
-    return jsonify(city.to_dict())
+    return jsonify(city.to_dict()), 200
